546fb7c26887a2423d69c87d2582297aca325414
Tyler Trinh <159205313+TylerTrinh12803@users.noreply.github.com>
2024-10-29 17:08:52 -0500

    Update PlaySong.jsx

--- client/src/pages/PlaySong.jsx
+++ client/src/pages/PlaySong.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactPlayer from 'react-player';
-import { useParams } from 'react-router-dom';
-
+import { useParams, useNavigate, Link } from 'react-router-dom';
 const apiUrl = process.env.REACT_APP_API_URL;
-
 const PlaySong = () => {
-    const { songId } = useParams();
-    const audioSrc = `${apiUrl}/artists/play/${songId}`;
+    const { songId, albumId } = useParams(); // Get songId and albumId from params
+    const audioSrc = `${apiUrl}/artists/play/${albumId}/${songId}`;
+    const navigate = useNavigate();
+
+    const handleGoHome = () => {
+        navigate('/'); // Navigate to the main page
+    };
+
+    useEffect(() => {
+        const incrementPlayCount = async () => {
+            try {
+                await fetch(`${apiUrl}/artists/songs/increment-play-count/${songId}`, {
+                    method: 'POST',
+                });
+            } catch (error) {
+                console.error("Error incrementing play count:", error);
+            }
+        };
+
+        incrementPlayCount(); // Call function to increment play count when the component mounts
+    }, [songId]); // Add songId to the dependency array
 
     return (
         <div style={{ textAlign: 'center', marginTop: '20px' }}>
@@ -19,6 +36,19 @@ const PlaySong = () => {
                 height='50px' 
             />
             <p style={{ marginTop: '10px' }}>Enjoy the music!</p>
+            <button className="cancel">
+                <Link to={`/albums/${albumId}/songs/${songId}`}>Back to Album</Link>
+            </button>
+            <p>
+                <button className="cancel">
+                    <Link to={`/search/song`}>Search Song</Link>
+                </button>
+            </p>
+            <div>
+                <p>
+                    <button className="cancel" onClick={handleGoHome}>Back Home</button>
+                </p>
+            </div>
         </div>
     );
 };
