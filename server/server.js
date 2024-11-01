fccf4244dc5463825873df44ae69d6969dbdf2f4
Andrew <andrewhbradley9@gmail.com>
2024-11-01 12:53:56 -0500

    update server.js

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.use('/artists', artistRoutes); // Ensure this matches your routes correctly
 app.use('/auth', authRoute);
 
 app.get('/', (req, res) => {
-    res.send("Checking leaf backend updates!");
+    res.send("Backend if finally CI/CD");
 });
 
 
